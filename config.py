e527976ea099781ce0bd9c7acb5a3245cbb7f8d5
JakobLindstrom <jakoblindstroem@gmail.com>
2024-11-24 17:39:12 +0100

    Removed channel_id

--- config.py
+++ config.py
@@ -7,6 +7,5 @@ with open('config.yaml', 'r') as file:
     config = yaml.safe_load(file)
 
 DISCORD_TOKEN = config['discord']['token']
-CHANNEL_ID = config['discord']['channel_id']
 PASSWORD = config['password']
 INITIAL_EXTENSIONS = ['cogs.games']
